7b5cc1def47c2f561dcbed4e45e1949ba33a016c
Andrew S <slip1410@gmail.com>
2022-11-15 16:33:23 +0200

    add pug engine

--- app.js
+++ app.js
@@ -4,19 +4,24 @@ const path = require('path')
 
 const app = express(bodyParser);
 
+app.engine('pug', require('pug').__express)
+
+app.set('view engine', 'pug');
+app.set('views', 'views');
+
 const rootDir = require('./util/path')
 
-const adminRoutes = require('./routes/admin');
+const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop')
 
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/admin', adminRoutes);
+app.use('/admin', adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-    res.status(404).sendFile(path.join(rootDir, 'views', '404.html'))
+    res.status(404).render('404')
 })
 
 app.listen(5000)
